94eba2de63629c39b47272b77b3e72dfae17c544
igrios <ignarios1@hotmail.com>
2023-02-22 15:59:24 -0300

    Merge remote-tracking branch 'origin/master'

---
+++ src/com/ignacio/rios/EjemploExecutor/EjemploExecutorProductorConsumidor.java
@@ -0,0 +1,30 @@
+package com.ignacio.rios.EjemploExecutor;
+
+import com.ignacio.rios.ejemplosSync.Panaderia;
+import com.ignacio.rios.ejemplosSync.runnable.Consumidor;
+import com.ignacio.rios.ejemplosSync.runnable.Panadero;
+
+import java.util.concurrent.*;
+
+public class EjemploExecutorProductorConsumidor {
+    public static void main(String[] args) throws InterruptedException,
+            ExecutionException, TimeoutException {
+
+      ThreadPoolExecutor exec = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
+
+
+     Panaderia p = new Panaderia();
+     Runnable productor = new Panadero(p);
+     Runnable consumidor = new Consumidor(p);
+
+        Future<?> futuro1 = exec.submit(productor);
+        Future<?> futuro2 = exec.submit(consumidor);
+
+
+
+
+        exec.shutdown();
+
+
+}
+}
